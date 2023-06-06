6c3f70fb285382712827be83d4903e2f552b9e1c
Gerardng1330 <gerar132000ng@gmail.com>
2023-06-06 11:22:27 -0500

    first commit

---
+++ ejercicio1.sql
@@ -0,0 +1,10 @@
+create database sqlserver
+
+/*Supongamos que estás trabajando en una empresa y te han solicitado realizar un seguimiento de los empleados y sus salarios. 
+Tienes una tabla llamada "Empleados" con las siguientes columnas: id, nombre, edad, salario y fecha_contratacion.
+
+Se te pide realizar las siguientes tareas:
+-Insertar al menos 5 registros de empleados en la tabla "Empleados" con información ficticia, asegurándote de incluir valores para todas las columnas.
+-Realizar una consulta SQL para obtener todos los empleados con un salario superior a $2000.
+-Realizar una consulta SQL para obteneer todos los empleados con un salario menor 1000.
+*/
